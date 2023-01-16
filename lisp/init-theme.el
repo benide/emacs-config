a523a1f6effe8926f8f1d8701a9a095090a4f1f3
Ben Ide <ben@bencide.com>
2023-01-15 20:45:59 -0500

    setup telephone-line to work with modus-themes v4

--- lisp/init-theme.el
+++ lisp/init-theme.el
@@ -19,30 +19,34 @@
   ;; (set-face-attribute 'fixed-pitch nil :family "Fira Code" :height 120)
   (when (> emacs-major-version 28)
     (set-face-attribute 'mode-line-active nil :inherit 'mode-line))
-  (load-theme 'modus-vivendi t)
-  ;; (set-face-attribute 'org-block nil :background "#191a1b")
-  ;; (set-face-attribute 'org-block-begin-line nil :background "#191a1b")
-  ;; (set-face-attribute 'org-block-end-line nil :background "#191a1b")
 
-  ;; (load-theme 'ef-bio)
+  (setq modus-themes-org-blocks 'gray-background)
+  (load-theme 'modus-vivendi t)
+
+  ;; copy of modus-themes v3 setup for telephone-line
+  (modus-themes-with-colors
+    (custom-set-faces
+     `(telephone-line-accent-active ((,c :background ,fg-mode-line-inactive :foreground ,bg-mode-line-inactive)))
+     `(telephone-line-accent-inactive ((,c :background ,bg-mode-line-active :foreground ,fg-mode-line-active)))
+     `(telephone-line-error ((,c :inherit bold :foreground ,modeline-err)))
+     `(telephone-line-evil ((,c :foreground ,fg-main)))
+     `(telephone-line-evil-emacs ((,c :inherit telephone-line-evil :background ,bg-magenta-intense)))
+     `(telephone-line-evil-insert ((,c :inherit telephone-line-evil :background ,bg-green-intense)))
+     `(telephone-line-evil-motion ((,c :inherit telephone-line-evil :background ,bg-yellow-intense)))
+
+     ;; color should be f0f0f0 with modus-operandi, 191a1b for modus-vivdendi. used to be caled bg-alt
+     ;; should I just choose bg-dim instead?
+     ;; `(telephone-line-evil-normal ((,c :inherit telephone-line-evil :background "#191a1b")))
+     `(telephone-line-evil-normal ((,c :inherit telephone-line-evil :background ,bg-dim)))
+
+     `(telephone-line-evil-operator ((,c :inherit telephone-line-evil :background ,bg-yellow-subtle)))
+     `(telephone-line-evil-replace ((,c :inherit telephone-line-evil :background ,bg-red-intense)))
+     `(telephone-line-evil-visual ((,c :inherit telephone-line-evil :background ,bg-cyan-intense)))
+     `(telephone-line-projectile ((,c :foreground ,modeline-info)))
+     `(telephone-line-unimportant ((,c :foreground ,fg-mode-line-inactive)))
+     `(telephone-line-warning ((,c :inherit bold :foreground ,modeline-warning)))))
 )
 
-;;; Colors from modus-themes 3.0 if we update to 4.0 at some point.
-;;; Stay on emacs 29 in the future to stay on modus-themes 3 and not worry about this
-    ;; `(telephone-line-accent-active ((,class :background ,fg-inactive :foreground ,bg-inactive)))
-    ;; `(telephone-line-accent-inactive ((,class :background ,bg-active :foreground ,fg-active)))
-    ;; `(telephone-line-error ((,class :inherit bold :foreground ,red-active)))
-    ;; `(telephone-line-evil ((,class :foreground ,fg-main)))
-    ;; `(telephone-line-evil-emacs ((,class :inherit telephone-line-evil :background ,magenta-intense-bg)))
-    ;; `(telephone-line-evil-insert ((,class :inherit telephone-line-evil :background ,green-intense-bg)))
-    ;; `(telephone-line-evil-motion ((,class :inherit telephone-line-evil :background ,yellow-intense-bg)))
-    ;; `(telephone-line-evil-normal ((,class :inherit telephone-line-evil :background ,bg-alt)))
-    ;; `(telephone-line-evil-operator ((,class :inherit telephone-line-evil :background ,yellow-subtle-bg)))
-    ;; `(telephone-line-evil-replace ((,class :inherit telephone-line-evil :background ,red-intense-bg)))
-    ;; `(telephone-line-evil-visual ((,class :inherit telephone-line-evil :background ,cyan-intense-bg)))
-    ;; `(telephone-line-projectile ((,class :foreground ,cyan-active)))
-    ;; `(telephone-line-unimportant ((,class :foreground ,fg-inactive)))
-    ;; `(telephone-line-warning ((,class :inherit bold :foreground ,yellow-active)))
 
 (defvar ide/is-theme-loaded nil)
 
